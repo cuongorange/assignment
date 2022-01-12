05f19da47f60735f58d7f33915bfca6c55105e64
cuongcam <cuongnqph14128@fpt.edu.vn>
2022-01-12 11:23:41 +0700

    update all

---
+++ src/components/footer.js
@@ -0,0 +1,6 @@
+const Footer = {
+    print(){
+        retunr ` <footer class="max-w-5xl text-center py-4 bg-blue-800 mx-auto > Nguyen Quoc Cuong  </footer> `
+    },
+};
+export default Footer;
---
+++ src/components/nav.js
@@ -0,0 +1,12 @@
+const nav ={
+    print(){
+        return /* html  */`
+        <nav>
+              <ul class =" flex ">
+               <li> <a href="/" class="block py-3 px-4"></a> HomePage </li>
+               <li> <a href="/about" class="block py-3 px-4"></a> AboutPage </li>
+               <li> <a href="/product" class="block py-3 px-4"></a> ProductPage </li>
+              </ul>
+        </nav>`
+    }
+}
